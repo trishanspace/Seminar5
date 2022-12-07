b9a82a7b6e9482dc10f6e87d78c7ba2fde2d5e7f
Nataliia <nataliyatrishechkina@mail.ru>
2022-12-07 14:59:54 +0300

    added code for task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,7 +3,7 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-int[] array = GenerateArray(10, 100, 5);
+int[] array = GenerateArray(-100, 100, 5);
 PrintArray(array);
 int sumElements = GetSumOfElementsOnOddIndexes(array);
 
@@ -12,14 +12,14 @@ void PrintArray(int[] array)
     Console.WriteLine($"[{string.Join(", ", array)}]");
 }
 
-Console.WriteLine($"Сумма элементов, стоящие на нечётных индексах равна {sumElements}.");
+Console.WriteLine($"Сумма элементов, стоящих на нечётных индексах равна {sumElements}.");
 
 int GetSumOfElementsOnOddIndexes(int[] array)
 {
     int sumElements = 0;
-    for (var i = 0; i < array.Length; i += 2)
+    for (var i = 1; i < array.Length; i +=2)
     {
-        if (array[i] >= 0)
+        if (array[i] > 0)
         {
             sumElements += array[i];
         }
